b89f2095074b27b32c2bcf7285d33e3bf1536dc6
nguyendoducthang09 <88147862+nguyendoducthang09@users.noreply.github.com>
2022-05-29 13:17:12 +0700

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,16 +1,14 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAcU6r15p8hgFIAjW6i8qkJ3S1KmxUfGK0",
-  authDomain: "todolist-b6475.firebaseapp.com",
-  projectId: "todolist-b6475",
-  storageBucket: "todolist-b6475.appspot.com",
-  messagingSenderId: "397045359173",
-  appId: "1:397045359173:web:b1f83f7bbb3ed11d39b92c",
-  measurementId: "G-QCP0TELQ5F"
+  apiKey: "AIzaSyBeY9aMI8nGoFnrIenTJTUh7pX2O2KGaDY",
+  authDomain: "todolist-df88b.firebaseapp.com",
+  databaseURL: "https://todolist-df88b-default-rtdb.firebaseio.com",
+  projectId: "todolist-df88b",
+  storageBucket: "todolist-df88b.appspot.com",
+  messagingSenderId: "449168465181",
+  appId: "1:449168465181:web:5fb96ec5e70e6b440695f4",
+  measurementId: "G-JPPEB82675"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 var db = firebase.firestore();
